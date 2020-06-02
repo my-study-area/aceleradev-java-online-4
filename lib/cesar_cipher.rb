8918a6547ac5cf3d75cb5727a2c79dbf0455fb2d
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-02 11:52:10 -0300

    adiciona método de descriptação

--- lib/cesar_cipher.rb
+++ lib/cesar_cipher.rb
@@ -13,4 +13,17 @@ class CesarCipher
         encripted_sentence
     end
 
+    def self.decrypt(sentence, rotation)
+        encripted_sentence = ''
+        sentence.downcase!
+        sentence.each_char do |char|
+            charcode = char.ord - rotation
+            case charcode
+            when 97..122 then encripted_sentence << (charcode).chr
+            when 71..96 then encripted_sentence << (charcode  + 26).chr
+            else encripted_sentence << char
+            end
+        end
+        encripted_sentence
+    end
 end
--- spec/cesar_cipher_spec.rb
+++ spec/cesar_cipher_spec.rb
@@ -24,4 +24,30 @@ describe 'cesar_cipher' do
         expect(CesarCipher.encript('a1a', 3)).to eq('d1d')
     end
   end
+
+  context '#decrypt' do
+    it 'o retorno é uma frase minúscula' do
+        expect(CesarCipher.decrypt("A", 2)).to match(/[a-z]/)
+        expect(CesarCipher.decrypt('C', 2)).to eq("a")
+    end
+
+    it 'descriptografa frase com salto de 1' do
+        expect(CesarCipher.decrypt('def', 1)).to eq('cde')
+    end
+
+    it 'reinicia alfabeto após a rotação passar da letra "Z"' do
+        expect(CesarCipher.decrypt'a', 1).to eq('z')
+        expect(CesarCipher.decrypt'z', 1).to eq('y')
+        expect(CesarCipher.decrypt'b', 1).to eq('a')
+        expect(CesarCipher.decrypt'nbdbdp', 1).to eq('macaco')
+    end
+
+    it 'não descriptografa ponto "."' do
+        expect(CesarCipher.decrypt('a.a', 3)).to eq('x.x')
+    end
+
+    it 'não decrypta ponto números' do
+        expect(CesarCipher.decrypt('a1a', 4)).to eq('w1w')
+    end
+  end
 end
