8e0f7024cb592db33da0b5ed4e00e2bb6fb1da47
Adriano Avelino <adriano-st777@hotmail.com>
2020-06-08 22:13:58 -0300

    cria o envio interativo

---
+++ app.rb
@@ -0,0 +1,116 @@
+require 'byebug'
+require 'rest-client'
+require 'json'
+require 'rickshaw'
+require 'date'
+require_relative 'token'
+require_relative 'lib/cesar_cipher'
+
+@option = "1"
+@token = ''
+@json = {}
+
+def show_header
+    puts '+' * 60
+    puts 'Desafio Java online'.center(60, '-')
+    puts 'Criptografia de Júlio César'.center(60,' ')
+    puts '=' * 60
+end
+
+def show_options
+    puts 'Selecione uma opção:'
+    puts '1 - Menu'
+    puts '2 - Mostrar frase descriptografada'
+    puts '3 - Enviar teste'
+    puts '0 - Sair'
+    @option = gets.chomp
+end
+
+def show_register_token
+    puts "TOKEN INVÁLIDO".center(60, '-')
+    puts "Registre o seu token"
+    puts "OBS: Para enviar o seu desafio você deve ter, em mãos, o seu"
+    puts "token.Para isso faça login no site https://codenation.dev/ e "
+    puts "clique na sua foto de perfil para visualizar o seu token"
+    print "Token: "
+    @token = gets.chomp
+end
+
+def show_decrypted_sentence
+    @json = Token.is_valid?(@token)
+    rotation = @json['numero_casas']
+    cifred_sentence = @json['cifrado']
+    decrypted_sentence = CesarCipher.decrypt(cifred_sentence, rotation)
+    puts "*".center(60,"*")
+    puts "A frase descriptografada é: #{decrypted_sentence[0..30]}"
+    puts "#{decrypted_sentence[31..-1]}"
+    puts "*".center(60,"*")
+end
+
+def show_send_test
+    last_sent = (File.exist?('log.txt'))? IO.readlines("log.txt")[0].to_i : -60
+    time_now = DateTime.now.strftime('%s').to_i
+
+    if time_now > last_sent + 60
+        Token.save_answer(@token)
+        json = Token.send_test(@token)
+        puts "*".center(60,"*")
+
+        if(JSON.parse(json)['code'] == '429')
+            puts "Você fez muitas requisições, aguarde alguns minutos"
+        else
+            score =  JSON.parse(json)['score']
+            puts "Teste enviado com sucesso!"
+            puts "Sua nota foi: " + score.to_s
+        end
+
+
+        puts "*".center(60,"*")
+        File.write("log.txt", DateTime.now.strftime('%s'))
+    else
+        puts "*".center(60,"*")
+        puts "Você só pode consultar um vez a cada minuto."
+        puts "Aguarde #{last_sent + 60 - time_now} segundos"
+        puts "*".center(60,"*")
+    end
+end
+
+def clear_terminal
+    system 'clear'
+end
+
+def token_is_valid?
+    return false unless Token.is_valid?(@token)
+    return true
+end
+
+while @option != "0"
+    
+    case @option
+    when "1"
+        clear_terminal()
+        show_header()
+        if token_is_valid?()
+            show_options()
+        else
+            show_register_token()
+        end
+    when "2"
+        clear_terminal()
+        show_header()
+        show_decrypted_sentence()
+        show_options()
+    when "3"
+        clear_terminal()
+        show_header()
+        show_send_test()
+        show_options()
+    else
+        clear_terminal()
+        show_header()
+        puts "*".center(60,"*")
+        puts "Opção inválida. Tente uma das opções abaixo."
+        puts "*".center(60,"*")
+        show_options()
+    end
+end
---
+++ token.rb
@@ -0,0 +1,52 @@
+class Token
+    def self.is_valid?(token) 
+        begin
+            url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=#{token}"
+            json = RestClient.get(url, headers={})
+        rescue RestClient::ExceptionWithResponse => e
+            json = e.response
+            e.response
+        end
+        return false if(JSON.parse(json)['code'] == 'not_found') 
+        JSON.parse(json)
+    end
+
+    def self.save_answer(token)
+        json = {}
+        begin
+            url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=#{token}"
+            json = RestClient.get(url, headers={})
+        rescue RestClient::ExceptionWithResponse => e
+            json = e.response
+            e.response
+        end
+        json_parsed = JSON.parse(json)
+        rotation = json_parsed['numero_casas']
+        cifred_sentence = json_parsed['cifrado']
+        decrypted_sentence = CesarCipher.decrypt(cifred_sentence, rotation)
+        json_parsed['decifrado'] = decrypted_sentence
+        json_parsed['resumo_criptografico'] = decrypted_sentence.to_sha1
+        File.open('./answer.json', 'w') do |line|
+            line.puts(json_parsed.to_json)
+        end
+        json_parsed
+    end
+
+    def self.send_test(token)
+        submition_url = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=#{token}"
+
+        begin
+            response = RestClient.post(
+                submition_url,
+                {
+                    multipart: true,
+                    answer: File.new("./answer.json", 'rb')
+                },
+                headers = {accept: :json}
+            )
+        rescue RestClient::TooManyRequests => e
+            response = e.response
+        end
+        response
+    end
+end
